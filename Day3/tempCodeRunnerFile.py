37f3947dea2102eb0d908d76d99957b6f547a2f6
Aakriti7 <you@example.com>
2025-08-14 17:54:10 +0545

    [update] updated my int.py

--- Day3/tempCodeRunnerFile.py
+++ Day3/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-list
+
+# num1 = int(input("Enter first integer: "))
+# num2 = int(input("Enter second integer: "))
+# print(f"The result is : {num1//num2}")
+
+
+# float1 = 40.0
+# int1 = int(float1)
+# print(f"The integer is: {int1}")
