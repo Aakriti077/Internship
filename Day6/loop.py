7ca9ca5ac7a27737395d311b0a89c9f207eed700
Aakriti7 <you@example.com>
2025-08-18 12:56:52 +0545

    [created] created loop.py and conditional.py

---
+++ Day6/loop.py
@@ -0,0 +1,25 @@
+#Print numbers from 1 to 10
+for i in range(1,11):
+    print(i)
+
+#Print all even numbers
+for i in range(2,51,2):
+    print(i)
+
+#Print the multiplication table of a number
+num=5
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
+
+#sum of first 100 natural numbers
+sum1=0
+for i in range(0,101):
+    sum1 += i
+print(sum1)
+
+#Print each character of a string
+word = "Python"
+for i in word:
+    print(i)
+
+
