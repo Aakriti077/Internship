119e62109eceb847416469920247a6b721edac3f
Aakriti7 <you@example.com>
2025-08-21 12:24:56 +0545

    [update] updated exception.py

---
+++ Day9/class.py
@@ -0,0 +1,33 @@
+class House:   #class
+    def __init__(self,color):    #constructor  , attribute
+        print("Inside class",self)    #self obj ho tya affai janxa
+        self.color = color
+
+    def ring_the_bell(self):   
+        print("Bell Ranggg",self)
+
+    def change_the_color(self,new_color):
+        self.color = new_color
+
+        
+
+house1 = House("Red")    #object
+# house1.color = "Blue"
+house2 = House("Yellow")
+
+print("House1 initial color: ",house1.color)
+print("House1 initial color: ",house2.color)
+
+#after change
+house1.change_the_color("Pink")
+house2.change_the_color("White")
+
+print("House1 new color: ",house1.color)
+print("House1 new color: ",house2.color)
+
+
+# print("House1 : ",house1.ring_the_bell())
+# # print(house1.color)
+# print("House2 : ",house2.ring_the_bell())
+
+
